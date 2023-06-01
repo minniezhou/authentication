0904b845b00fdfe0488771da7ff60d03ec92e0cd
min zhou <min.zhou@tableau.com>
2023-06-01 09:52:38 -0400

    pipeline

---
+++ cmd/api/handler_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Test handler", func() {
+	When("Acess auth", func() {
+		It("should be successful", func() {
+			fact := true
+			Expect(fact).To(BeTrue())
+		})
+	})
+})
